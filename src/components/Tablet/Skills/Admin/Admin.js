162ad4983867662229c415286b98de2e86fc68e5
davidthomashess <davidthomashess@gmail.com>
2022-11-08 18:57:31 -0600

    update skills buttons to be function based components

--- src/components/Tablet/Skills/Admin/Admin.js
+++ src/components/Tablet/Skills/Admin/Admin.js
@@ -1,13 +1,13 @@
 import './Admin.css'
 
 const Admin = () => {
-  return <div class="container-tablet">
-  <div class="inner-container-tablet">
-      <div class="sub-skill-1-tablet">
-        <div class="inner-sub-skill-1-header-tablet">
+  return <div className="container-tablet">
+  <div className="inner-container-tablet">
+      <div className="sub-skill-1-tablet">
+        <div className="inner-sub-skill-1-header-tablet">
           <h2>Admin</h2>
         </div>
-        <div class="inner-sub-skill-1-content-tablet">
+        <div className="inner-sub-skill-1-content-tablet">
           <ul>
             <li>[Tablettttt] and more details</li>
             <li>Some more details</li>
@@ -18,11 +18,11 @@ const Admin = () => {
           </ul>
         </div>
       </div>
-      <div class="sub-skill-2-tablet">
-        <div class="inner-sub-skill-2-header-tablet">
+      <div className="sub-skill-2-tablet">
+        <div className="inner-sub-skill-2-header-tablet">
           <h2>Skill</h2>
         </div>
-        <div class="inner-sub-skill-2-content-tablet">
+        <div className="inner-sub-skill-2-content-tablet">
           <ul>
             <li>Details and more details</li>
             <li>Some more details</li>
@@ -33,11 +33,11 @@ const Admin = () => {
           </ul>
         </div>
       </div>
-      <div class="sub-skill-3-tablet">
-        <div class="inner-sub-skill-3-header-tablet">
+      <div className="sub-skill-3-tablet">
+        <div className="inner-sub-skill-3-header-tablet">
           <h2>Skill</h2>
         </div>
-        <div class="inner-sub-skill-3-content-tablet">
+        <div className="inner-sub-skill-3-content-tablet">
           <ul>
             <li>Details and more details</li>
             <li>Some more details</li>
@@ -48,11 +48,11 @@ const Admin = () => {
           </ul>
         </div>
       </div>
-      <div class="sub-skill-4-tablet">
-        <div class="inner-sub-skill-4-header-tablet">
+      <div className="sub-skill-4-tablet">
+        <div className="inner-sub-skill-4-header-tablet">
           <h2>Skill</h2>
         </div>
-        <div class="inner-sub-skill-4-content-tablet">
+        <div className="inner-sub-skill-4-content-tablet">
           <ul>
             <li>Details and more details</li>
             <li>Some more details</li>
--- src/components/Tablet/Skills/Skills.js
+++ src/components/Tablet/Skills/Skills.js
@@ -9,55 +9,22 @@ import Terminal from './Terminal/Terminal'
 import Testing from './Testing/Testing'
 import Web from './Web/Web'
 
-import Tabs from './Tabs'
 
 const Skills = () => {
   return (
     <div className="innerContent">
       <div className="innerContentButtons">
-        <Tabs>
-          <div label="code">
-            <Code />
-          </div>
-          <div label="terminal">
-            <Terminal />
-          </div>
-          <div label="web">
-            <Web />
-          </div>
-          <div label="testing">
-            <Testing />
-          </div>
-          <div label="admin">
-            <Admin />
-          </div>
-          <div label="agile">
-            <Agile />
-          </div>
-          <div label="git">
-            <Git />
-          </div>
-          <div label="databases">
-            <Databases />
-          </div>
-        </Tabs>
-      </div>
+          <Code />
+          <Terminal />
+          <Web />
+          <Testing />
+          <Admin />
+          <Agile />
+          <Git />
+          <Databases />
+        </div>
     </div>
   )
-  
-  /*
-    code
-    shell
-    git
-
-    agile
-    testing
-    sharepoint
-
-    data
-    admin
-    api
-  */
 }
 
 export default Skills
