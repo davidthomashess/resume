df43068be0cce9380fdf74fe79eaf8db3138ccd9
davidthomashess <davidthomashess@gmail.com>
2022-11-08 23:38:21 -0600

    updated Tablet skills section to use functional components and repositioned the buttons

--- src/components/Tablet/Skills/Skills.js
+++ src/components/Tablet/Skills/Skills.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react'
+
 import './Skills.css'
 
 import Code from './Code/Code'
@@ -11,17 +13,114 @@ import Web from './Web/Web'
 
 
 const Skills = () => {
+  const [page, setPage] = useState()
+  
+  const active = 'active-tab-color-tablet skillButton-tablet'
+  const inactive = 'inactive-tab-color-tablet skillButton-tablet'
+  
+  const [codeActive, setCodeActive] = useState(inactive)
+  const [terminalActive, setTerminalActive] = useState(inactive)
+  const [webActive, setWebActive] = useState(inactive)
+  const [testingActive, setTestingActive] = useState(inactive)
+  const [adminActive, setAdminActive] = useState(inactive)
+  const [agileActive, setAgileActive] = useState(inactive)
+  const [gitActive, setGitActive] = useState(inactive)
+  const [databaseActive, setDatabaseActive] = useState(inactive)
+  
+  const options = {
+    code: "code",
+    terminal: "terminal",
+    web: "web",
+    testing: "testing",
+    admin: "admin",
+    agile: "agile",
+    git: "git",
+    database: "database"
+  }
+  
+  const activeSwitcher = (option) => {
+    setCodeActive(() => option === options.code ? active : inactive)
+    setTerminalActive(() => option === options.terminal ? active : inactive)
+    setWebActive(() => option === options.web ? active : inactive)
+    setTestingActive(() => option === options.testing ? active : inactive)
+    setAdminActive(() => option === options.admin ? active : inactive)
+    setAgileActive(() => option === options.agile ? active : inactive)
+    setGitActive(() => option === options.git ? active : inactive)
+    setDatabaseActive(() => option === options.database ? active : inactive)
+  }
+  
+  const code = () => {
+    activeSwitcher(options.code)
+    setPage(
+      <Code />
+    )
+  }
+  
+  const terminal = () => {
+    activeSwitcher(options.terminal)
+    setPage(
+      <Terminal />
+    )
+  }
+  
+  const web = () => {
+    activeSwitcher(options.web)
+    setPage(
+      <Web />
+    )
+  }
+  
+  const testing = () => {
+    activeSwitcher(options.testing)
+    setPage(
+      <Testing />
+    )
+  }
+  
+  const admin = () => {
+    activeSwitcher(options.admin)
+    setPage(
+      <Admin />
+    )
+  }
+  
+  const agile = () => {
+    activeSwitcher(options.agile)
+    setPage(
+      <Agile />
+    )  
+  }
+  
+  const git = () => {
+    activeSwitcher(options.git)
+    setPage(
+      <Git />
+    )
+  }
+  
+  const databases = () => {
+    activeSwitcher(options.database)
+    setPage(
+      <Databases />
+    )
+  }
+  
   return (
-    <div className="innerContent">
-      <div className="innerContentButtons">
-          <Code />
-          <Terminal />
-          <Web />
-          <Testing />
-          <Admin />
-          <Agile />
-          <Git />
-          <Databases />
+    <div className="innerContent-tablet">
+      <div className="innerContentButtons-tablet">
+        <div className="skillButtons-rows-tablet">
+          <div className={codeActive} onClick={code}>Code</div>
+          <div className={terminalActive} onClick={terminal}>Terminal</div>
+          <div className={webActive} onClick={web}>Web</div>
+          <div className={testingActive} onClick={testing}>Testing</div>
+        </div>
+        <div className="skillButtons-rows-tablet">
+          <div className={adminActive} onClick={admin}>Admin</div>
+          <div className={agileActive} onClick={agile}>Agile</div>
+          <div className={gitActive} onClick={git}>Git</div>
+          <div className={databaseActive} onClick={databases}>Database</div>
+        </div>
+          {page}
         </div>
     </div>
   )
