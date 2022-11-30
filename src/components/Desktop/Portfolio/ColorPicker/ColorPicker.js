dff4426c0f4eb35d3842e9a7ba4e79383d4df96c
davidthomashess <davidthomashess@gmail.com>
2022-11-29 18:08:28 -0600

    feat: add a color picker app

--- src/components/Desktop/Portfolio/ColorPicker/ColorPicker.js
+++ src/components/Desktop/Portfolio/ColorPicker/ColorPicker.js
@@ -1,73 +1,113 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './ColorPicker.css'
 
+const hexHash=/^#([0-9a-f]{3}){1,2}$/i;
+
 const ColorPicker = () => {
-  document.body.style = 'background-color: red;';
+  const handleFocusRGB = (event) => event.target.select()
   
   const [rgb, setRgb] = useState([127, 127, 127])
-  const [hexes, setHexes] = useState(["7F", "7F", "7F"])
+  const [hexes, setHexes] = useState(["7f", "7f", "7f"])
+  const [hexColor, setHexColor] = useState(["#7f7f7f"])
+  const [hexInputSwitch, setHexInputSwitch] = useState(800)
   
-  const redText = (val) => {
-    const hex = val.target.value < 16 
-      ? `0${Number.parseInt(val.target.value, 10).toString(16)}` 
-      : Number.parseInt(val.target.value, 10).toString(16)  
-    const num = parseInt(Number.parseInt(val.target.value, 10)) || 0
-    
-    setHexes(previousHexes => Object.assign([], previousHexes, {0: hex}))
-    setRgb(previousRgb => Object.assign([], previousRgb, {0: num}))
-    
+  useEffect(() => {
     let newBgColor = document.getElementsByClassName("desktopColorDisplay")
     for(let i = 0; i < newBgColor.length; i++) {
       newBgColor[i].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
     }
+    setHexInputSwitch(0)
+    setHexColor(`#${hexes[0]}${hexes[1]}${hexes[2]}`)
+  }, [rgb, hexes])
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if(hexColor.toString().match(hexHash)) {
+        const redHex = ((hexColor).toString().slice(1)).match(/.{1,2}/g)[0]
+        const grnHex = ((hexColor).toString().slice(1)).match(/.{1,2}/g)[1]
+        const bluHex = ((hexColor).toString().slice(1)).match(/.{1,2}/g)[2]
+        
+        const redNum = parseInt(redHex, 16)
+        const grnNum = parseInt(grnHex, 16)
+        const bluNum = parseInt(bluHex, 16)
+        
+        setHexes([redHex, grnHex, bluHex])
+        setRgb([redNum, grnNum, bluNum])
+      } else {
+        setHexColor('#HEX_ONLY')
+      }
+    }, hexInputSwitch)
+
+    return () => clearTimeout(timer)
+  }, [hexColor, hexInputSwitch])
+  
+  const HexColorAndHexDelayOn = (val) => {
+    setHexInputSwitch(800)
+    setHexColor(val)
+  }
+  
+  const redText = (val) => {
+    
+    const hex = val.target.value < 16 
+      ? `0${
+          isNaN(Number.parseInt(val.target.value, 10).toString(16)) 
+          ? '0'
+          : (Number.parseInt(val.target.value, 10).toString(16))
+        }` 
+      : Number.parseInt(val.target.value, 10).toString(16)  
+    const num = parseInt(Number.parseInt(val.target.value, 10)) || 0
+    
+    setHexes(previousHexes => Object.assign([], previousHexes, {0: num <= 255 ? hex : 'ff'}))
+    setRgb(previousRgb => Object.assign([], previousRgb, {0: num <= 255 ? num : 255}))
   }
   
   const greenText = (val) => {
     const hex = val.target.value < 16 
-      ? `0${Number.parseInt(val.target.value, 10).toString(16)}` 
-      : Number.parseInt(val.target.value, 10).toString(16)
-      const num = parseInt(Number.parseInt(val.target.value, 10)) || 0
+      ? `0${
+          isNaN(Number.parseInt(val.target.value, 10).toString(16)) 
+          ? '0'
+          : (Number.parseInt(val.target.value, 10).toString(16))
+        }` 
+      : Number.parseInt(val.target.value, 10).toString(16)  
+    const num = parseInt(Number.parseInt(val.target.value, 10)) || 0
     
-    setHexes(previousHexes => Object.assign([], previousHexes, {1: hex}))
-    setRgb(previousRgb => Object.assign([], previousRgb, {1: num}))
-    
-    let newBgColor = document.getElementsByClassName("desktopColorDisplay")
-    for(let i = 0; i < newBgColor.length; i++) {
-      newBgColor[i].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
-    }
+    setHexes(previousHexes => Object.assign([], previousHexes, {1: num <= 255 ? hex : 'ff'}))
+    setRgb(previousRgb => Object.assign([], previousRgb, {1: num <= 255 ? num : 255}))
   }
   
   const blueText = (val) => {
-    const hex = val.target.value < 16
-      ? `0${Number.parseInt(val.target.value, 10).toString(16)}` 
-      : Number.parseInt(val.target.value, 10).toString(16)
-      const num = parseInt(Number.parseInt(val.target.value, 10)) || 0
+    const hex = val.target.value < 16 
+      ? `0${
+          isNaN(Number.parseInt(val.target.value, 10).toString(16)) 
+          ? '0'
+          : (Number.parseInt(val.target.value, 10).toString(16))
+        }` 
+      : Number.parseInt(val.target.value, 10).toString(16)  
+    const num = parseInt(Number.parseInt(val.target.value, 10)) || 0
     
-    setHexes(previousHexes => Object.assign([], previousHexes, {2: hex}))
-    setRgb(previousRgb => Object.assign([], previousRgb, {2: num}))
-    
-    let newBgColor = document.getElementsByClassName("desktopColorDisplay")
-    for(let i = 0; i < newBgColor.length; i++) {
-      newBgColor[i].style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
-    }
+    setHexes(previousHexes => Object.assign([], previousHexes, {2: num <= 255 ? hex : 'ff'}))
+    setRgb(previousRgb => Object.assign([], previousRgb, {2: num <= 255 ? num : 255}))
   }
   
-  const hexDispenser = (val) => {
-    const redHex = ((val.target.value).toString().slice(1)).match(/.{1,2}/g)[0]
-    const grnHex = ((val.target.value).toString().slice(1)).match(/.{1,2}/g)[1]
-    const bluHex = ((val.target.value).toString().slice(1)).match(/.{1,2}/g)[2]
-    
-    const redNum = parseInt(redHex, 16)
-    const grnNum = parseInt(grnHex, 16)
-    const bluNum = parseInt(bluHex, 16)
-    
-    setHexes([redHex, grnHex, bluHex])
-    setRgb([redNum, grnNum, bluNum])
-    
-    const myReference = this.desktopColorDisplay.current // The DOM element
-    myReference.style.backgroundColor = `rgb(${redNum}, ${grnNum}, ${bluNum})`; // The style you want to apply
-  }
+  // const hexDispenser = (val) => {
+  //   clearTimeout(timer)
+
+  //   const newTimer = setTimeout(() => {
+  //     const redHex = ((val.target.value).toString().slice(1)).match(/.{1,2}/g)[0]
+  //     const grnHex = ((val.target.value).toString().slice(1)).match(/.{1,2}/g)[1]
+  //     const bluHex = ((val.target.value).toString().slice(1)).match(/.{1,2}/g)[2]
+      
+  //     const redNum = parseInt(redHex, 16)
+  //     const grnNum = parseInt(grnHex, 16)
+  //     const bluNum = parseInt(bluHex, 16)
+      
+  //     setHexes([redHex, grnHex, bluHex])
+  //     setRgb([redNum, grnNum, bluNum])
+  //   }, 500)
+
+  //   setTimer(newTimer)
+  // }
   
   console.log(document.body.style.backgroundColor);
   
@@ -95,6 +135,7 @@ const ColorPicker = () => {
             size="3" 
             value={rgb[0]}
             onChange={redText} 
+            onFocus={handleFocusRGB}
           />
         </div>
       </div>
@@ -117,6 +158,7 @@ const ColorPicker = () => {
             size="3" 
             value={rgb[1]}
             onChange={greenText}
+            onFocus={handleFocusRGB}
           />
         </div>
       </div>
@@ -139,16 +181,17 @@ const ColorPicker = () => {
             value={rgb[2]}
             size="3"
             onChange={blueText}
+            onFocus={handleFocusRGB}
           />
         </div>
       </div>
       <div>
         <input 
           className="desktop-hex-io"
-          value={`#${hexes[0]}${hexes[1]}${hexes[2]}`}
-          onChange={hexDispenser}
+          value={hexColor}
+          onChange={val => HexColorAndHexDelayOn(val.target.value)}
         />
-        <input className="desktop-cp-button" type="button" value="copy" />
+        <input className="desktop-cp-button" type="button" onClick={() =>  navigator.clipboard.writeText(`#${hexes[0]}${hexes[1]}${hexes[2]}`)} value="copy" />
       </div>
     </div>
   )
